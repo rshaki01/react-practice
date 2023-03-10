06afa47e6d3a4db65841b6fc31a08fd6913b32e3
rshaki01 <rshaki01@gmail.com>
2023-03-10 00:15:34 -0500

    Added markdown files

---
+++ nextjs-blog/pages/_app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/globals.css'
+
+const _app = ({ Component, pageProps }) => {
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+export default _app
